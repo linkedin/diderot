53ae7a67aef5e022b47bc695ffea133ee343e220
PapaCharlie <paul.chesnais@gmail.com>
2025-04-02 09:30:21 -0700

    Use OSS lib

--- internal/cache/resource_map.go
+++ internal/cache/resource_map.go
@@ -3,14 +3,14 @@ package internal
 import (
 	"iter"
 
-	"github.com/puzpuzpuz/xsync/v3"
+	"github.com/puzpuzpuz/xsync/v4"
 )
 
 // ResourceMap is a concurrency-safe map. It deliberately does not expose bare Get or Put methods as
 // its concurrency model is based on the assumption that access to the backing values must be
 // strictly synchronized. Instead, all operations should be executed through the various Compute
 // methods.
-type ResourceMap[K comparable, V any] xsync.MapOf[K, V]
+type ResourceMap[K comparable, V any] xsync.Map[K, V]
 
 func NewResourceMap[K comparable, V any]() *ResourceMap[K, V] {
 	return (*ResourceMap[K, V])(xsync.NewMapOf[K, V]())
@@ -24,10 +24,10 @@ func (m *ResourceMap[K, V]) Compute(
 	newValue func(key K) V,
 	compute func(value V),
 ) (v V, created bool) {
-	v, _ = (*xsync.MapOf[K, V])(m).ComputeV2(key, func(v V, loaded bool) (_ V, op xsync.ComputeOp) {
+	v, _ = (*xsync.Map[K, V])(m).Compute(key, func(v V, loaded bool) (_ V, op xsync.ComputeOp) {
 		if !loaded {
 			v = newValue(key)
-			op = xsync.Update
+			op = xsync.UpdateOp
 			created = true
 		}
 		compute(v)
@@ -39,7 +39,7 @@ func (m *ResourceMap[K, V]) Compute(
 // ComputeIfPresent invokes the given function only if a corresponding entry exists in the map for
 // the given key.
 func (m *ResourceMap[K, V]) ComputeIfPresent(key K, f func(value V)) (wasPresent bool) {
-	(*xsync.MapOf[K, V])(m).ComputeV2(key, func(oldValue V, loaded bool) (_ V, op xsync.ComputeOp) {
+	(*xsync.Map[K, V])(m).Compute(key, func(oldValue V, loaded bool) (_ V, op xsync.ComputeOp) {
 		if !loaded {
 			return oldValue, op
 		}
@@ -56,13 +56,13 @@ func (m *ResourceMap[K, V]) ComputeIfPresent(key K, f func(value V)) (wasPresent
 // otherwise nothing happens. As a "compute" function, the condition is executed synchronously, in
 // other words, it is guaranteed that no other "compute" functions are executing on that entry.
 func (m *ResourceMap[K, V]) ComputeDeletion(key K, condition func(value V) (deleteEntry bool)) (deleted bool) {
-	(*xsync.MapOf[K, V])(m).ComputeV2(key, func(oldValue V, loaded bool) (_ V, op xsync.ComputeOp) {
+	(*xsync.Map[K, V])(m).Compute(key, func(oldValue V, loaded bool) (_ V, op xsync.ComputeOp) {
 		if !loaded {
 			return oldValue, op
 		}
 
 		if condition(oldValue) {
-			op = xsync.Delete
+			op = xsync.DeleteOp
 			deleted = true
 		}
 		return oldValue, op
@@ -73,10 +73,10 @@ func (m *ResourceMap[K, V]) ComputeDeletion(key K, condition func(value V) (dele
 
 // Range returns an [iter.Seq2] that will iterate over all entries in this map.
 func (m *ResourceMap[K, V]) Range() iter.Seq2[K, V] {
-	return (*xsync.MapOf[K, V])(m).Range
+	return (*xsync.Map[K, V])(m).Range
 }
 
 // Size returns the current number of entries in the map.
 func (m *ResourceMap[K, V]) Size() int {
-	return (*xsync.MapOf[K, V])(m).Size()
+	return (*xsync.Map[K, V])(m).Size()
 }

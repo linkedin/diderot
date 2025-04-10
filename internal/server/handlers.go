8c88bdeb911a40551d05d6291b32800868d07e20
Abhishek Bharadwaj <abharadwaj@linkedin.com>
2025-04-10 15:31:59 -0700

    adding logs

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"context"
+	"log/slog"
 	"maps"
 	"sync"
 	"time"
@@ -332,6 +333,8 @@ func (h *handler) EndNotificationBatch() {
 func (h *handler) handleDeletionsFromIRV() {
 	for name, irv := range h.initialResourceVersions {
 		if _, ok := h.entries[name]; !ok && !irv.received {
+			slog.Debug("Resource no longer exists on the server but is still present on the client.
+			 Explicitly marking the resource for deletion.", "resourceName", name)
 			h.entries[name] = nil
 		}
 	}
@@ -341,6 +344,7 @@ func (h *handler) handleDeletionsFromIRV() {
 func (h *handler) handleMatchFromIRV(name string, r *ads.RawResource) bool {
 	if res, ok := h.initialResourceVersions[name]; ok {
 		if r != nil && res.version == r.Version {
+			slog.Debug("resource version matches with IRV from client, skipping this resource", "resourceName", name, "version", res.version)
 			res.received = true
 			return true
 		}
